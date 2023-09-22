b251b5c36ca84ef890e85ea3d99b2f5dfcf01dac
Jozimar Queiroz <jozimarjo@gmail.com>
2023-09-22 13:06:04 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
   title = 'test';
+  constructor(private service: TestService){}
+  ngOnInit(): void {
+    this.service.example().subscribe(v=>{
+      console.log('teste',v)
+    })
+  }
+
 }
