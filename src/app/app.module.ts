2353d48a69aa6e05efa3a464991da0865512f519
jozimarjo@gmail.com <Jozimar Queiroz>
2023-09-22 13:06:04 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './features/home/home.component';
+import { CartComponent } from './features/cart/cart.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
